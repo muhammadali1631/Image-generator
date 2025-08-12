178eb458625aba3beb23a367e9965dbb3bf7f4f8
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2025-08-12 12:10:39 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
       {
         headers: {
-          Authorization: `Bearer ${api_key}`,
+          Authorization: `Bearer ${process.env.api_key}`,
           "Content-Type": "application/json",
         },
         method: "POST",
